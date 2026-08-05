0bdc516ad0043eb92711c0d081aaf488519fe4cc
nd6u!W <1109467689@qq.com>
2026-08-05 19:34:36 +0800

    Fix URL syntax in focusmic.rb (#1)
    Remove the extra backslash so the version is interpolated in the FocusMic release asset URL.

--- Casks/focusmic.rb
+++ Casks/focusmic.rb
@@ -2,7 +2,7 @@ cask "focusmic" do
   version "0.0.4"
   sha256 "8d4533c388ffdda2473a5a20281523d010f6f1a45ab516883c3b8e027eba83da"
 
-  url "https://github.com/lageev/FocusMic/releases/download/v#{version}/FocusMic-\#{version}.zip",
+  url "https://github.com/lageev/FocusMic/releases/download/v#{version}/FocusMic-#{version}.zip",
       verified: "github.com/lageev/FocusMic/"
   name "FocusMic"
   desc "Keep the default audio input locked to your preferred microphone"
