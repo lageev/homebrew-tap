d74c520b9d8218a7126d41dd1ad6c1cbfb48dcd6
fring <fring@fringmini.local>
2026-09-15 16:01:08 +0800

    Update Studio Mini to 0.2.4

--- Casks/studio-mini.rb
+++ Casks/studio-mini.rb
@@ -1,10 +1,10 @@
 cask "studio-mini" do
-  version "0.2.3"
-  sha256 "a970f80b1d72c0e4bf1e6aafaec72dff3667549154efa1a07a03419e9089de2b"
+  version "0.2.4"
+  sha256 "b39645083c0671b4d9e679dca562c693ef6d81ee0f92ddbc63480fa5dcd99a0c"
 
   url "https://github.com/lageev/StudioMini/releases/download/v#{version}/Studio-Mini-#{version}.zip"
   name "Studio Mini"
-  desc "Build, install, and manage Android APKs from macOS"
+  desc "Build, install, and manage Android APKs"
   homepage "https://github.com/lageev/StudioMini"
 
   auto_updates true
